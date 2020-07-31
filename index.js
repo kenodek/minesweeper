ca1ef866a986246cf4867ff17f812008f327e5c1
kenodek <dominik.targosz@protonmail.com>
2020-07-31 11:27:26 +0200

    Prepared to deploy

--- index.js
+++ index.js
@@ -20,4 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/user", userRouter);
 
-app.listen(5000, () => console.log("Server is listening..."));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => console.log("Server is listening..."));
